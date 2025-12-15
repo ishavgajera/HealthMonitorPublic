aaa07fe4d0a87abca5a64bc45dfffd77b6938180
IshaGajera7 <ivg6@scarletmail.rutgers.edu>
2025-12-15 13:45:58 -0500

    updated health monitor public version

---
+++ HealthMonitor.java
@@ -0,0 +1,29 @@
+/**
+* Health Monitor has students implement 1D arrays as a way to store and analyze a patient's health metrics. 
+* The overall goal is to determine whether the patient is healthy by calculating their Health Score,
+* and comparing it to predetermined ranges. 
+* Write the HealthMonitor program inside the main method as per the assignment description. 
+*
+* Input Format: 
+* | Age | Sleep Duration | Quality of Sleep | Physical Activity Level | Stress Level | Heart Rate | Daily Steps | Sex | BMI | BP | Sleep Disorder |
+*
+* Compilation: javac healthMonitor.java
+* Execution: java healthMonitor <input metrics>
+*
+* @author Isha Gajera
+* @author Anna Lu
+*/
+public class HealthMonitor {
+
+    // Amount of each type of metric. Use these to initialize your arrays.
+    static final int DOUBLE_METRICS        = 7;
+    static final int CHAR_METRICS          = 4;
+
+    public static void main(String[] args) {
+
+        // WRITE YOUR CODE HERE
+
+
+
+    }
+}
